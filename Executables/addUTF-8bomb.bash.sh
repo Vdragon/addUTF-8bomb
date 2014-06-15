962555b50f324c6d63ff72e3b4aea74bb56cdde0
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-06-15 12:19:40 +0800

    Script - 以下修正：
    * 將部份變數以雙引號括住避免被當作多個參數
    * 程式碼美觀修正

--- Executables/addUTF-8bomb.bash.sh
+++ Executables/addUTF-8bomb.bash.sh
@@ -8,17 +8,22 @@ fi
 
 for file in $*;
 do
-  echo "# Processing: $file" 1>&2
-  if [ ! -f "$file" ];
-    then
-      echo Not a file: "$file" 1>&2
-      continue
-  fi
-  TYPE=`file - < "$file" | cut -d: -f2`
-  if echo "$TYPE" | grep -q '(with BOM)';
-    then
-      echo "# $file already has BOM, skipping." 1>&2
-    else
-      ( mv ${file} ${file}~ && uconv -f utf-8 -t utf-8 --add-signature < "${file}~" > "${file}" ) || ( echo Error processing "$file" 1>&2 ; exit 1)
-  fi
+	echo "# Processing: $file" 1>&2
+	if [ ! -f "$file" ];
+		then
+			echo Not a file: "$file" 1>&2
+			continue
+	fi
+	TYPE=`file - < "$file" | cut -d: -f2`
+	if echo "$TYPE" | grep -q '(with BOM)';
+		then
+			echo "# $file already has BOM, skipping." 1>&2
+		else
+			( mv "${file}" "${file}"~ 
+				&& uconv -f utf-8 -t utf-8 --add-signature 
+					< "${file}~" 
+					> "${file}" )
+				|| ( echo Error processing "$file" 1>&2 ; 
+				exit 1)
+	fi
 done
